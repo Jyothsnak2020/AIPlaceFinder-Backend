1d831acc3bf8d9d10810da962e74ff034f3281eb
JYOTHSNA KANCHARLA <127539292+Jyothsnak2020@users.noreply.github.com>
2025-01-31 23:19:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = 5000;
 
 // Use CORS middleware
 app.use(cors({
-  origin: 'https://dapper-fenglisu-d63e99.netlify.app', // Replace with your React app's URL
+  origin: 'https://cheery-croquembouche-9b74c9.netlify.app', // Replace with your React app's URL
 }));
 
 app.use(bodyParser.json());
